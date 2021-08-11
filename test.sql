c660c7d00a92e13d96c7817980d74b08829a537e
김나래 <narae900@naver.com>
2021-08-11 22:27:22 +0900

    미니프로젝트

--- test.sql
+++ test.sql
@@ -1,3 +1,5 @@
+
+
 -- 테이블 정보 확인
 select * from tab;
 
@@ -31,3 +33,26 @@ values(2, '김비트', '010-8413-3266', '032-1964-2744');
 select * from PHONE_BOOK;
 
 commit;
+
+
+create table PhoneBook (
+    id number(10),
+    name VARCHAR2(10),
+    hp VARCHAR2(20),
+    tel VARCHAR2(20),
+    PRIMARY key (id)
+);
+
+CREATE SEQUENCE seq_phonebook;
+DESC PhoneBook;
+
+
+INSERT into PhoneBook (id, name, hp, tel)
+values(1, '강비트', '010-1178-6877', '02-1587-5817');
+
+select * from PhoneBook;
+
+commit;
+
+INSERT into PhoneBook (id, name, hp, tel)
+values(2, '윤비트', '010-8879-3266', '032-4487-2777');
