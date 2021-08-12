d1858ceb2ac70e5c11c81b71b6e8c9c2933d15b4
김나래 <narae900@naver.com>
2021-08-12 17:36:21 +0900

    21.08.12 수정

--- test.sql
+++ test.sql
@@ -3,38 +3,7 @@
 -- 테이블 정보 확인
 select * from tab;
 
--- 미니 프로젝트 테이블 만들기
-create table PHONE_BOOK (
-    id number(10),
-    name VARCHAR2(10),
-    hp VARCHAR2(20),
-    tel VARCHAR2(20),
-    PRIMARY key (id)
-);
-
-CREATE SEQUENCE seq_phone_book;
-
-
---확인
-DESC PHONE_BOOK;
-
-SELECT id, name, hp, tel  FROM PHONE_BOOK;
-
-INSERT into PHONE_BOOK (id, name, hp, tel)
-values(1, '김나래', '010-2375-9999', '02-334-4871');
-
-select * from PHONE_BOOK;
-
-commit;
-
-INSERT into PHONE_BOOK (id, name, hp, tel)
-values(2, '김비트', '010-8413-3266', '032-1964-2744');
-
-select * from PHONE_BOOK;
-
-commit;
-
-
+-- 테이블 만들기
 create table PhoneBook (
     id number(10),
     name VARCHAR2(10),
@@ -56,3 +25,16 @@ commit;
 
 INSERT into PhoneBook (id, name, hp, tel)
 values(2, '윤비트', '010-8879-3266', '032-4487-2777');
+
+commit;
+
+
+
+select max(id) from PhoneBook; -- 3부터 시작 pk 중복 x
+
+create SEQUENCE seq_id
+    start with 3
+    INCREMENT by 1
+    MAXVALUE 1000000;
+
+commit;
